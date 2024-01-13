0f106257d94330a4c5865a56478f810a399c9bbc
AlbertZaqaryan <itm.albert.zaqaryan@gmail.com>
2024-01-13 18:17:38 +0400

    v1

---
+++ ZoravarAndranik/ZoravarAndranik/Controllers/DzukController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ZoravarAndranik.Data;
+using ZoravarAndranik.Models;
+
+namespace ZoravarAndranik.Controllers
+{
+    [Route("tandz/xndzor-mandarin, shnorhavorenq dzer nor tarin")]
+    [ApiController]
+    public class DzukController : Controller
+    {
+
+        private readonly AppDbContext _context;
+        public DzukController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public List<Dzuk> Index()
+        {
+            var data = _context.Dzuks.ToList();
+            return data;
+        }
+
+        [HttpPost]
+        public void AddSomething(Dzuk dzuk)
+        {
+
+            _context.Dzuks.Add(dzuk);
+            _context.SaveChanges();
+        }
+
+       
+
+        
+
+    }
+}
---
+++ ZoravarAndranik/ZoravarAndranik/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ZoravarAndranik.Models;
+
+namespace ZoravarAndranik.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Dzuk> Dzuks { get; set; }
+    }
+}
